6d0edfe5b7588de12c2c3a4d2bb49141e0173dae
JohnTrunix <john@trunix.ch>
2023-03-12 16:14:39 +0100

    Refactored and optimized main program, added new Python files for better code organization

---
+++ runner.py
@@ -0,0 +1,246 @@
+# autopep8: off
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+import random
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
+from dotenv import load_dotenv
+import subprocess
+
+from runner_utils import InfluxDB_Writer, plot_one_box
+from influxdb_client import Point
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0] / 'Yolov7_StrongSORT_OSNet'
+WEIGHTS = FILE.parents[0] / 'weights'
+
+#---------------------- Add Paths for custom modules ----------------------#
+
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+if str(ROOT / 'yolov7') not in sys.path:
+    sys.path.append(str(ROOT / 'yolov7'))  # add yolov5 ROOT to PATH
+if str(ROOT / 'strong_sort') not in sys.path:
+    sys.path.append(str(ROOT / 'strong_sort'))  # add strong_sort ROOT to PATH
+if str(ROOT / 'strong_sort' / 'deep' / 'reid') not in sys.path:
+    # add reid ROOT to PATH
+    sys.path.append(str(ROOT / 'strong_sort' / 'deep' / 'reid'))
+
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+
+from Yolov7_StrongSORT_OSNet.strong_sort.strong_sort import StrongSORT
+from Yolov7_StrongSORT_OSNet.strong_sort.utils.parser import get_config
+from Yolov7_StrongSORT_OSNet.yolov7.models.experimental import attempt_load
+from Yolov7_StrongSORT_OSNet.yolov7.utils.datasets import (LoadImages, LoadStreams)
+from Yolov7_StrongSORT_OSNet.yolov7.utils.general import (check_img_size, non_max_suppression, scale_coords, xyxy2xywh)
+from Yolov7_StrongSORT_OSNet.yolov7.utils.torch_utils import (select_device, time_synchronized)
+
+load_dotenv()
+VID_FORMATS = 'asf', 'avi', 'gif', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'ts', 'wmv'
+
+
+@torch.no_grad()
+def run_moni(
+        stream_id,
+        source,
+        yolo_weights,
+        strong_sort_weights,
+        mp_barrier=None,  # multiprocessing barrier object for synchronization
+        device='0',
+        imgsz=(640, 640),
+        conf_thres=0.25,
+        iou_thres=0.45,
+        classes=[0],  # only detect and track persons (class 0)
+        rtmp_enable=False,  # enable output stream to rtmp server
+        rtmp_url=None,  # rtmp server url
+        show_vid=True,  # shows video ouput with cv2
+        show_plot=False,  # live plot output
+        save_influx=False,  # save data to influxdb
+        line_thickness=2,  # bounding boxes line thickness
+        hide_labels=False,
+        hide_conf=True,
+        hide_class=False,
+        half=False  # convert 32-bit tensor to 16-bit
+):
+
+    # initialize InfluxDB_writer if save_influx is True
+    if save_influx:
+        '''
+        ToDo: Load Config from .yml file
+        '''
+        influx_writer = InfluxDB_Writer(
+            url=os.getenv('INFLUXDB_URL'),
+            token=os.getenv('INFLUXDB_TOKEN'),
+            org=os.getenv('INFLUXDB_ORG'),
+            bucket=os.getenv('INFLUXDB_BUCKET')
+        )
+
+    # initialize rtmp stream writer if rtmp_enable is True and rtmp_url is not None
+    if rtmp_enable:
+        if rtmp_url is None:
+            raise ValueError('rtmp_url is None')
+        else:
+            '''
+            ToDo: Load Config from .yml file
+            '''
+            rtmp_process = subprocess.Popen(
+                ['ffmpeg', '-y', '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24', '-s', f'{imgsz[0]}x{imgsz[1]}', '-i', '-', '-f', 'flv', rtmp_url], stdin=subprocess.PIPE)
+
+    # setup configs for stream processing
+    is_file = Path(source).suffix[1:] in VID_FORMATS
+    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+    is_webcam = source.isnumeric() or (is_url and not is_file)
+
+    # load yolov7 model
+    device = select_device(device)
+    WEIGHTS.mkdir(parents=True, exist_ok=True)
+    model = attempt_load(Path(yolo_weights), map_location=device)
+    names = model.names
+    stride = model.stride.max().cpu().numpy()
+    imgsz = check_img_size(imgsz, s=stride)
+
+    # load strong sort model
+    '''
+    ToDo: Correctly load strong sort model and make detections shared between processes
+    '''
+    cfg = get_config()
+    strong_sort = StrongSORT(cfg, Path(strong_sort_weights))
+    outputs = [None]
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # load stream or video/images
+    if is_webcam:
+        cudnn.benchmark = True
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    #---------------------- RUN MONITORING LOOP FOR EACH FRAME ----------------------#
+    dt, seen = [0.0, 0.0, 0.0, 0.0], 0
+    curr_frames, prev_frames = [None], [None]
+
+    for frame_idx, (path, im, im0s, vid_cap) in enumerate(dataset):
+        s = ''
+        t1 = time_synchronized()
+        im = torch.from_numpy(im).to(device)
+        im = im.half() if half else im.float()
+        im /= 255.0
+        if len(im.shape) == 3:
+            im = im[None]
+        t2 = time_synchronized()
+        dt[0] += t2 - t1
+
+        #---------------------- YOLOv7 DETECTIONS ----------------------#
+        pred = model(im)
+        t3 = time_synchronized()
+        dt[1] += t3 - t2
+
+        #---------------------- Non Max Suppression ----------------------#
+        pred = non_max_suppression(pred[0], conf_thres, iou_thres, classes)
+        t4 = time_synchronized()
+        dt[2] += t4 - t3
+
+        #---------------------- Process detections ----------------------#
+        for i, det in enumerate(pred):
+            seen += 1
+
+            if is_webcam:
+                p, im0, _ = Path(path[i]), im0s[i].copy(), dataset.count[i]
+                s += f'{i}: '
+            else:
+                p, im0, _ = Path(path), im0s.copy(), getattr(
+                    dataset, 'frame', 0)
+
+            curr_frames[i] = im0
+            s += f'{im.shape[2]}x{im.shape[3]} '
+
+            '''
+            ToDo: with .yaml config file
+            '''
+            if (os.getenv('STRONGSORT_ECC') == 'True'):
+                strong_sort.tracker.camera_update(
+                    prev_frames[i], curr_frames[i])
+
+            if det is not None and len(det):
+                det[:, :4] = scale_coords(
+                    im.shape[2:], det[:, :4], im0.shape).round()
+
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()
+                    s += f'{n} {names[int(c)]}{"s" * (n > 1)}, '
+
+                xywhs = xyxy2xywh(det[:, :4])
+                confs = det[:, 4]
+                clss = det[:, 5]
+
+                t5 = time_synchronized()
+                outputs[i] = strong_sort.update(
+                    xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
+                t6 = time_synchronized()
+                dt[3] += t6 - t5
+
+                #---------------------- Draw detections ----------------------#
+                if len(outputs[i]) > 0:
+                    for j, (output, conf) in enumerate(zip(outputs[i], confs)):
+                        bboxes = output[0: 4]
+                        id = output[4]
+                        cls = output[5]
+
+                        c = int(cls)
+                        id = int(id)
+                        label = None if hide_labels else (f'{id} {names[c]}' if hide_conf else
+                                                          (f'{id} {conf:.2f}' if hide_class else f'{id} {names[c]} {conf:.2f}'))
+                        plot_one_box(bboxes, im0, label=label, color=colors[int(
+                            cls)], line_thickness=line_thickness)
+
+                        #---------------------- Save to InfluxDB ----------------------#
+                        if save_influx:
+                            point = Point('person').tag('stream', stream_id
+                                                        ).tag('frame', frame_idx
+                                                              ).tag('id', id
+                                                                    ).field('x', int((bboxes[0] + bboxes[2]) / 2)
+                                                                            ).field('y', int(bboxes[3]))
+                            influx_writer.add(point)
+
+                print(
+                    f'{s}Done. YOLOv7:({t3 - t2:.3f}s) NMS:({t4 - t3:.3f}s) SORT:({t6 - t5:.3f}s)')
+
+                if save_influx:
+                    influx_writer.write()
+
+            else:
+                strong_sort.increment_ages()
+                print('No detections')
+
+            if show_vid:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)
+
+            if rtmp_enable:
+                fb = im0.tostring()
+                rtmp_process.stdin.write(fb)
+
+            if show_plot:
+                '''
+                Not Implemented yet: Plotting of detections in images and global coordinates
+                '''
+                pass
+
+        #---------------------- Update previous frames ----------------------#
+            prev_frames[i] = curr_frames[i]
+
+        if mp_barrier is not None:
+            mp_barrier.wait()
+
+    if rtmp_enable:
+        rtmp_process.stdin.close()
+        rtmp_process.wait()
+
+    if save_influx:
+        influx_writer.close()
---
+++ runner_utils.py
@@ -0,0 +1,80 @@
+import sys
+import random
+import cv2
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+import pkg_resources
+
+
+class InfluxDB_Writer:
+    '''
+    Simple class to write data to influxdb
+
+    :param url: str - influxdb url
+    :param token: str - influxdb token
+    :param org: str - influxdb org
+    :param bucket: str - influxdb bucket
+
+    :return: None
+    '''
+
+    def __init__(self, url: str, token: str, org: str, bucket: str):
+        self.client = InfluxDBClient(url=url, token=token, org=org)
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+        self.org = org
+        self.bucket = bucket
+        self.points = []
+
+    def add(self, point: Point):
+        self.points.append(point)
+
+    def write(self):
+        self.write_api.write(bucket=self.bucket,
+                             org=self.org, record=self.points)
+        self.points.clear()
+
+    def close(self):
+        try:
+            self.client.close()
+        except Exception as e:
+            print(e)
+
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=3):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(
+        0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+
+    # plot dot in lower center to indicate ground position
+    cv2.circle(img, (int((x[0] + x[2]) / 2), int(x[3])),
+               8, color, thickness=1, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3,
+                    [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
+def check_packages(requirements_path):
+    with open(requirements_path, 'r') as f:
+        required_packages = f.readlines()
+
+    missing_packages = []
+
+    for package in required_packages:
+        package = package.strip()
+        try:
+            pkg_resources.require(package)
+        except pkg_resources.DistributionNotFound:
+            missing_packages.append(package)
+
+    if missing_packages:
+        print(
+            f"ERROR: The following packages are missing: {', '.join(missing_packages)}")
+    else:
+        print("All required packages are present in the runtime environment.")
